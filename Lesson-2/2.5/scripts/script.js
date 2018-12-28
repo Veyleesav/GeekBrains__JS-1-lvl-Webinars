f3aa6cfc867b5cfd86e64f1f5374621b1cce8e17
Lev Vasilyev <wanbergg@gmail.com>
2018-12-28 10:50:35 +0300

    2.5
    task done

---
+++ Lesson-2/2.5/scripts/script.js
@@ -0,0 +1,18 @@
+/*
+5) Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
+ */
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
