7b868276be6f3150490f383bfdae75e76460b189
Lev Vasilyev <wanbergg@gmail.com>
2019-01-11 19:37:29 +0300

    fix
    misunderstood task

--- Lesson-3/3.1/scripts/script.js
+++ Lesson-3/3.1/scripts/script.js
@@ -1,5 +1,26 @@
 let count=0;
 while (count<=100){
+    if (count <= 1) {
+        console.log(count);
+        count++;
+        continue;
+    } else if (count <= 3) {
+        console.log(count);
+        count++;
+        continue;
+    } else if (count%2 === 0 || count%3 === 0) {
+        count++;
+        continue;
+    }
+
+    let i = 5;
+    while (i*i <= count) {
+        if (count%i === 0 || count%(i+2) === 0) {
+            count++;
+            continue;
+        }
+        i += 6
+    }
     console.log(count);
-    count++
+    count++;
 }
