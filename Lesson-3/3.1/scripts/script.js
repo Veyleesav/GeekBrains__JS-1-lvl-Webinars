bc281b59ec91f9c42dd69b40f6f5aeb16f0947bf
Lev Vasilyev <wanbergg@gmail.com>
2019-01-15 12:08:13 +0300

    3.1 fixed
    to be better and clearer

--- Lesson-3/3.1/scripts/script.js
+++ Lesson-3/3.1/scripts/script.js
@@ -1,26 +1,17 @@
-let count=0;
-while (count<=100){
-    if (count <= 1) {
-        console.log(count);
-        count++;
-        continue;
-    } else if (count <= 3) {
-        console.log(count);
-        count++;
-        continue;
-    } else if (count%2 === 0 || count%3 === 0) {
-        count++;
-        continue;
+let num=2;
+while (num<=100){
+    if (isSimple(num)){
+        console.log(num);
     }
-
-    let i = 5;
-    while (i*i <= count) {
-        if (count%i === 0 || count%(i+2) === 0) {
-            count++;
-            continue;
-        }
-        i += 6
-    }
-    console.log(count);
-    count++;
+    num++
+}
+function isSimple(num) {
+   let i=2;
+   while (i<num){
+       if(num%2===0){
+           return false;
+       }
+       i++
+   }
+   return true;
 }
